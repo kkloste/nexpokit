ec4cbb069fb2b44febd804be87652ba444b3733a
David Gleich <dgleich@purdue.edu>
2014-03-14 14:02:39 -0400

    Updates for new sparesheap, google dense map, etc.

--- sparseheap.hpp
+++ sparseheap.hpp
@@ -3,100 +3,78 @@
  * A set of functions to manipulate a max-heap as a set of 3 hashtables
  */
 
-#ifdef __APPLE__
-#include <tr1/unordered_set>
-#include <tr1/unordered_map>
-#define tr1ns std::tr1
-#else
-#include <unordered_set>
-#include <unordered_map>
-#define __STDC_UTF_16__ 1
-#define tr1ns std
-#endif
+#include <sparsehash/dense_hash_map>
+#include <vector>
+#include <stdlib.h>
+#include <limits>
 
-
-struct sparsemaxheap {
-    typedef unsigned int uint;
-    typedef tr1ns::unordered_map< uint, double > map_type;
-    typedef tr1ns::unordered_map< uint, uint > T_map;
-    typedef tr1ns::unordered_map< int, uint > L_map;
-    map_type map;
-    T_map T;
-    L_map L;
-    unsigned int hsize;
+template< typename index_type, typename value_type, class local_index_type = size_t > 
+struct sparse_max_heap 
+{
+    // setup the data structure
+    // index_to_lindex takes the original, sparse indices, and maps them
+    // to a local index in our array. 
     
-    /** Remove max entry in heap, replace with last entry
-     *  then heap down. Sets val = maxval, returns index of maxval
-     */
-    uint extractmax(double &val){
-        // what if hsize = 0 ?
-        /*        if (hsize==0){
-         return -1;
-         }*/
-        uint oldind = T[0];
-        val = map[oldind];
-        map[oldind] = 0;
+    typedef google::dense_hash_map<index_type, local_index_type> local_map;
+    local_map index_to_lindex;
+    local_index_type nextind;
+    std::vector< index_type > lindex_to_index;
+    
+    // all of these vectors use our linear index
+    std::vector< value_type > values;
+    std::vector< local_index_type > T;
+    std::vector< local_index_type > L; // L[i] is the location of item i.
+    size_t hsize;
+    
+    // set a sentinal. 
+    const static local_index_type lastval = std::numeric_limits<local_index_type>::max();  
+    
+    index_type extractmax(value_type &val) {
+        local_index_type lindmax = T[0];
+        index_type rval = lindex_to_index[lindmax];
+        val = values[lindmax];
         T[0] = T[hsize-1]; // move the last entry to the root
-        L[T[0]] = 0;        //update L to reflect this
-        //            map.erase(T[hsize-1]); // erase that entry from the heap
-        //            T.erase(hsize-1);
-        //            L.erase(oldind);
-        L[oldind] = -1;
-        hsize--;
-        heap_down(0);   //then heap down to re-heapify.
-        return oldind;
+        L[T[0]] = 0;       // update the index to reflect this
+        L[lindmax] = lastval; // set the location o the last index to be zero.
+        hsize --;
+        heap_down(0);
+        return (rval);
     }
     
-    void heap_down(uint k){
-        uint heapk = T[k];
+    
+    void heap_down(local_index_type k){
+        local_index_type heapk = T[k];
         while (1) {
-            uint i=2*(k+1)-1;
+            local_index_type i=2*(k+1)-1;
             if (i>=hsize) { break; } /* end of heap */
             if (i<hsize-1) {
                 /* pick between children (unneeded if i==heap_size-1) */
-                uint left=T[i];
-                uint right=T[i+1];
-                if (map[right] > map[left]) {
+                local_index_type left=T[i];
+                local_index_type right=T[i+1];
+                if (values[right] > values[left]) {
                     i=i+1; /* pick the larger child */
                 }
             }
-            if (map[heapk] > map[T[i]]) {
+            if (values[heapk] > values[T[i]]) {
                 /* k is larger than both children, so end */
                 break;
             } else {
-                T[k] = T[i]; L[T[i]]=k;
-                T[i] = heapk; L[heapk] = i;
+                T[k] = T[i]; 
+                L[T[i]]=k;
+                T[i] = heapk; 
+                L[heapk] = i;
                 k=i;
             }
         }
     }
-    
-    /** When altering an entry of map, this checks
-     *  whether that entry is already in map, or if a new entry
-     *  need be made in the heap before adding 'value' to map[index].
-     */
-    void update(uint index, double value){
-        if ( L.count(index) == 0 || L[index] == -1){
-            // if 'index' was never in L, or if it was but has been removed: insert new entry
-            map[index] = value;
-            T[hsize] = index;
-            L[index] = hsize;
-            hsize++;
-            heap_up(hsize-1);
-        }
-        else{ // update old entry
-            map[index] += value;
-            heap_up(L[index]);
-        }
-    }
-    
-    uint heap_up(uint j){
+        
+    local_index_type heap_up(local_index_type j){
         while (1) {
             if (j==0) { break; } /* the element is at the top */
-            uint heapj = T[j];
-            uint j2 = (j-1)/2;  /* otherwise, compare with its parent */
-            uint heapj2 = T[j2];
-            if (map[heapj2] > map[heapj]) {
+            local_index_type heapj = T[j];
+            local_index_type j2 = (j-1)/2;  /* otherwise, compare with its parent */
+            local_index_type heapj2 = T[j2];
+            if (values[heapj2] > values[heapj]) {
                 break; /* the parent is larger, so stop */
             } else {
                 /* the parent is smaller, so swap */
@@ -106,5 +84,59 @@ struct sparsemaxheap {
             }
         }
         return j;
+    }
+    
+    /** When altering an entry of map, this checks
+     *  whether that entry is already in map, or if a new entry
+     *  need be made in the heap before adding 'value' to map[index].
+     */
+    void update(index_type index, value_type value) {
+        local_index_type lindex; 
+        
+        // check if we need to grow our vectors
+        if ( index_to_lindex.count(index) == 0 ) {
+            // we have not seen this entry before, and we need
+            // to create a local index for it.
+            if (nextind >= lindex_to_index.size()) {
+                // this means that we need to grow all of the arrays
+                size_t nextsize = 10*lindex_to_index.size();
+                lindex_to_index.resize(nextsize);
+                values.resize(nextsize);
+                T.resize(nextsize);
+                L.resize(nextsize);
+            }
+            // create the entries of the map
+            lindex = nextind;    
+            index_to_lindex[index] = lindex;
+            lindex_to_index[lindex] = index;
+            L[lindex] = lastval;
+            
+            // set the nextval to be one larger
+            nextind ++ ; 
+        } else {
+            lindex = index_to_lindex[index];
+        }
+
+        if ( L[lindex] == lastval ){
+            // if 'index' was never in L, or 
+            // if it was but has been removed: 
+            // insert new entry at the end
+            values[lindex] = value;
+            T[hsize] = lindex;
+            L[lindex] = hsize;
+            hsize++;
+            heap_up(hsize-1);
+        }
+        else{ // update old entry
+            values[lindex] += value;
+            heap_up(L[lindex]);
+        }
+    }
+    
+    sparse_max_heap(size_t initial_size) 
+    : nextind(0), hsize(0), values(initial_size), L(initial_size), 
+        lindex_to_index(initial_size), T(initial_size)
+    { 
+        index_to_lindex.set_empty_key(std::numeric_limits<index_type>::max()); 
     }
 };
