f97dd90131936b09a672b6e8d8eccd3cfcab8a05
Kyle Kloster <kyle.kloster@gmail.com>
2014-03-19 14:11:57 -0400

    debuggin new version of gexpmq_mex.cpp

--- gexpmq_mex.cpp
+++ gexpmq_mex.cpp
@@ -40,14 +40,13 @@ int debugflag = 0;
  * @param nsteps - the number of output steps (length 1)
  */
 void gexpmq(const mwSize n, const mwIndex* cp, const mwIndex* ari, const double* a, 
-            const mwIndex c, const mwIndex N,  const double tol, const mwIndex maxsteps, 
+            const mwIndex c, const double t,  const double tol, const mwIndex maxsteps, 
             double* y, double *nsteps, double *npushes)
 {
 DEBUGPRINT(("Input n=%i N=%i c=%i tol=%i maxsteps=%i\n", n, N, c, tol, maxsteps));
+	mwIndex N = (mwIndex)taylordegree(t,tol);
     mwIndex M = n*N;
     double sumresid = 0.;
-    double tolnN = tol/(n*N);
-
 	// compute the tolerances for the components of the residual
     std::vector<double> psivec(N+1,0.);
     psivec[N] = 1;
@@ -55,9 +54,9 @@ DEBUGPRINT(("Input n=%i N=%i c=%i tol=%i maxsteps=%i\n", n, N, c, tol, maxsteps)
         psivec[N-k] = psivec[N-k+1]*t/(double)(N-k+1) + 1;
     } // psivec[k] = psi_k(t)
 
-    pushcoeff[0] = (exp(t)*eps/(double)N)/psivec[0];
+    pushcoeff[0] = (exp(t)*tol/(double)N)/psivec[0];
 	// This is a more numerically stable way to compute
-	//      pushcoeff[j] = exp(t)*eps/(N*psivec[j])
+	//      pushcoeff[j] = exp(t)*tol/(N*psivec[j])
 	for (lindex k = 1; k <= N ; k++){
 		pushcoeff[k] = pushcoeff[k-1]*(psivec[k-1]/psivec[k]);
 	}
@@ -150,7 +149,7 @@ DEBUGPRINT(("Before for-loop\n"));
 }
 
 // USAGE: [expmv nstep npush] = gexpmq_mex(A,c,tol)
-// optional: gexpmq_mex(A,c,tol,maxsteps,N)
+// optional: gexpmq_mex(A,c,tol,t,maxsteps)
 void mexFunction(
   int nargout, mxArray *pargout[],       // these are your outputs
   int nargin, const mxArray *pargin[])   // these are your arguments
@@ -159,23 +158,21 @@ void mexFunction(
     // A - sparse n-by-n
     // c - integer scalar 1 <= c <= n
     // tol - double value, 0 < tol < Inf
+    // t = doublevalue, 0 < t < inf
     // maxsteps - integer scalar max-steps 
-    // optional: N - integer scalar 1 <= N <= Inf
-    // if N is not given, function 'tayordegree' is
-    // called to set N
     const mxArray* A = pargin[0];
     mwSize n = mxGetM(A);
     mwIndex c = (mwIndex)mxGetScalar(pargin[1])-1;
     double tol = mxGetScalar(pargin[2]);
-    mwIndex maxsteps = 10*n;
+    double t = 1.;
+    mwIndex maxsteps = 1000*n;
     if (nargin >= 4){
-        maxsteps = (mwIndex)mxGetScalar(pargin[3]);
+        t = mxGetScalar(pargin[3]);
+    }
+    if (nargin >= 5){
+        maxsteps = (mwIndex)mxGetScalar(pargin[4]);
     }
 
-    mwIndex N = (mwIndex)taylordegree(1.0,tol);
-    if (nargin == 5){
-        N = (mwIndex)mxGetScalar(pargin[4]);
-    }
 DEBUGPRINT(("declare outputs\n"));
     pargout[0] = mxCreateDoubleMatrix(n,1,mxREAL);
     pargout[1] = mxCreateDoubleMatrix(1,1,mxREAL);
@@ -192,10 +189,6 @@ DEBUGPRINT(("get sparsemat ptrs \n"));
     
 DEBUGPRINT(("Starting call \n"));
     
-    if ( N <= 0 ){
-        mexErrMsgIdAndTxt("gexpmq_mex:wrongInputParamterN",
-                          "gexpmq_mex needs N > 0");
-    }
     if ( tol <= 0 || tol > 1){
         mexErrMsgIdAndTxt("gexpmq_mex:wrongInputParamterTol",
                           "gexpmq_mex needs 0 < tol <= 1");
