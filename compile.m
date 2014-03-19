878c72df9a4c8d26dcca451ebc8ee933d3adae80
Kyle Kloster <kyle.kloster@gmail.com>
2014-03-19 12:55:18 -0400

    update compile to work with new expm_svec

--- compile.m
+++ compile.m
@@ -3,7 +3,7 @@ mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims gsqres_mex.cpp
 mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -largeArrayDims gexpm_mex.cpp
 mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims gexpmq_mex.cpp
 mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims gexpm_hash_mex.cpp
-mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -largeArrayDims expm_svec_mex.cpp
+mex -O CXXFLAGS="\$CXXFLAGS -Wall -std=c++0x" -I. -largeArrayDims expm_svec_mex.cpp
 %mex -O CXXFLAGS="\$CXXFLAGS -Wall" -largeArrayDims hpexpm_mex.cpp
 %mex -O CXXFLAGS="\$CXXFLAGS -Wall" -largeArrayDims gexpmq_mex.cpp
 
