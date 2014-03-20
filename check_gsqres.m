20d886beae3cc65629ee50c723276af25e11dd82
David Gleich <dgleich@purdue.edu>
2014-03-20 14:23:48 -0400

    Added new code to simplify residual check

--- check_gsqres.m
+++ check_gsqres.m
@@ -5,5 +5,6 @@ Z = expm(P);
 tol = 1e-4;
 for j=1:size(P,1)
     [y npush] = gsqres_mex(P,j,tol/2,1.,0); % this should be full accuracy
+    %[y npush] = gexpmq_mex(P,j,tol/2,1.); % this should be full accuracy
     assert(norm(y-Z(:,j),1) < tol, sprintf('failed on column %i of netscience ', j))
 end
