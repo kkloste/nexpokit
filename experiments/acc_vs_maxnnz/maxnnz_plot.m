11ca065494efb7a2800ab3f7760adcabe9faef01
Kyle Kloster <kyle.kloster@gmail.com>
2014-04-01 23:57:55 -0400

    maxnnz plotting scripts improved

--- experiments/acc_vs_maxnnz/maxnnz_plot.m
+++ experiments/acc_vs_maxnnz/maxnnz_plot.m
@@ -1,30 +1,96 @@
 experimentname = 'acc_maxnnz';
 experiment_directory = '/scratch2/dgleich/kyle/nexpokit/results/';
 
+addpath('~/nexpokit/plotting');
+
 load(strcat(experiment_directory, experimentname, '_to_plot') );
 
 % For each value of topks = [25,100,1000]
 % try each pair: {1-norm, kendall, intersect} vs {maxnnz, maxnnz/edgedensity}
 
-[num_metrics, num_data, num_maxnnzs, num_topks] = size(acc_Data);
+[num_metrics, num_data, num_maxnnzs, num_topks] = size(acc_data);
 acc_xaxis = zeros(num_maxnnzs,1);
 maxnnzs = [100,200,500,1000,2000,5000,10000];
 topks = [25,100,1000];
 
-%% PLOT : log10(1-norm) (metricsid =1)
+figdims = [3,4];
+
+%% PLOT : log10(1-norm / edgedensity) (metricsid =1)
+clf;
 hold all
 metricsid = 1; % use 1-norm
 topksid = 1; % doesn't matter, since 1-norm is same for all
 for dataid=1:num_data
-    plot(maxnnzs, squeeze(acc_data(metricsid,dataid,:,topksid)));
+    xaxis = log10(maxnnzs);
+%    xaxis = log10(maxnnzs./edgedensity(dataid));
+%    xaxis = maxnnzs;
+%    xaxis = maxnnzs./edgedensity(dataid);
+    plot(xaxis, squeeze(acc_data(metricsid,dataid,:,topksid)) - log10(edgedensity(dataid)) );
 end
 
+ylim([-5.5,-1]);
+title('expmimv: log10(1-norm error / edge-density) vs. log10(maxnnz)');
+xlabel('log10(maxnnz)');
+ylabel('log10(1-norm error / edge-density)');
+legend('itdk0304', 'dblp-2010', 'flickr-scc', 'ljournal-2008', 'webbase-2001','twitter-2010','friendster','Location','Northeast');
+legend boxoff;
+set_figure_size(figdims);
+print(gcf,strcat('maxnnz_vs_norm_edense','.eps'),'-depsc2');
+
+%% PLOT : log10(1-norm) (metricsid =1)
+clf;
+hold all
+metricsid = 1; % use 1-norm
+topksid = 1; % doesn't matter, since 1-norm is same for all
+for dataid=1:num_data
+    xaxis = log10(maxnnzs);
+%    xaxis = log10(maxnnzs./edgedensity(dataid));
+%    xaxis = maxnnzs;
+%    xaxis = maxnnzs./edgedensity(dataid);
+    plot(xaxis, squeeze(acc_data(metricsid,dataid,:,topksid)) );
+end
+
+ylim([-5.5,-1]);
+title('expmimv: log10(1-norm error / edge-density) vs. log10(maxnnz)');
+xlabel('log10(maxnnz)');
+ylabel('log10(1-norm error / edge-density)');
+legend('itdk0304', 'dblp-2010', 'flickr-scc', 'ljournal-2008', 'webbase-2001','twitter-2010','friendster','Location','Northeast');
+legend boxoff;
+set_figure_size(figdims);
+print(gcf,strcat('maxnnz_vs_norm_edense','.eps'),'-depsc2');
+
+
 %% PLOT : kendall (metricsid = 2)
+clf;
 hold all
 metricsid = 2; % usekendall
-topksid = 2; % 25,100,1000
+topksid = 3; % 25,100,1000
 xlineorig = topks(topksid);
-ylim([0,1]);
+for dataid=1:num_data
+    xaxis = log10(maxnnzs); xline = log10(xlineorig);
+%    xaxis = log10(maxnnzs./edgedensity(dataid)); xline = log10(xlineorig);
+%    xaxis = maxnnzs; xline = xlineorig;
+%    xaxis = maxnnzs./edgedensity(dataid); xline = xlineorig;
+    plot(xaxis, squeeze(acc_data(metricsid,dataid,:,topksid)));    
+end
+hx = graph2d.constantline(xline, 'LineStyle','--','Color',[.7 .7 .7]);
+changedependvar(hx,'x');
+
+title('expmimv: Kendall Tau vs. log10(maxnnz)');
+xlabel('log10(maxnnz)');
+ylabel('Kendall Tau');
+legend('itdk0304', 'dblp-2010', 'flickr-scc', 'ljournal-2008', 'webbase-2001','twitter-2010','friendster','Location','Southeast');
+legend boxoff;
+set_figure_size(figdims);
+print(gcf,strcat('maxnnz_vs_Kendall','.eps'),'-depsc2');
+
+
+%% PLOT : kendall edense (metricsid = 2)
+clf;
+hold all
+metricsid = 2; % usekendall
+topksid = 3; % 25,100,1000
+xlineorig = topks(topksid);
 for dataid=1:num_data
 %    xaxis = log10(maxnnzs); xline = log10(xlineorig);
     xaxis = log10(maxnnzs./edgedensity(dataid)); xline = log10(xlineorig);
@@ -35,13 +101,48 @@ end
 hx = graph2d.constantline(xline, 'LineStyle','--','Color',[.7 .7 .7]);
 changedependvar(hx,'x');
 
+title('expmimv: Kendall Tau vs. log10(maxnnz/edgedensity)');
+xlabel('log10(maxnnz/edgedensity)');
+ylabel('Kendall Tau');
+legend('itdk0304', 'dblp-2010', 'flickr-scc', 'ljournal-2008', 'webbase-2001','twitter-2010','friendster','Location','Southeast');
+legend boxoff;
+set_figure_size(figdims);
+print(gcf,strcat('maxnnz_vs_Kendall_edense','.eps'),'-depsc2');
+
 
 %% PLOT : intersect (metricsid = 3)
+clf;
 hold all
 metricsid = 3; % use intersect
 topksid = 3; % 25,100,1000
 xlineorig = topks(topksid);
-ylim([0,1]);
+% ylim([0,1]);
+for dataid=1:num_data
+   xaxis = log10(maxnnzs); xline = log10(xlineorig); 
+%    xaxis = log10(maxnnzs./edgedensity(dataid)); xline = log10(xlineorig);
+%    xaxis = maxnnzs; xline = xlineorig;
+%    xaxis = maxnnzs./edgedensity(dataid); xline = xlineorig;
+    plot(xaxis, squeeze(acc_data(metricsid,dataid,:,topksid)));    
+end
+hx = graph2d.constantline(xline, 'LineStyle','--','Color',[.7 .7 .7]);
+changedependvar(hx,'x');
+
+title('expmimv: Set Precision vs. log10(maxnnz)');
+xlabel('log10(maxnnz)');
+ylabel('Set Precision');
+legend('itdk0304', 'dblp-2010', 'flickr-scc', 'ljournal-2008', 'webbase-2001','twitter-2010','friendster','Location','Southeast');
+legend boxoff;
+set_figure_size(figdims);
+print(gcf,strcat('maxnnz_vs_Precision','.eps'),'-depsc2');
+
+
+%% PLOT : intersect w edge density (metricsid = 3)
+clf;
+hold all
+metricsid = 3; % use intersect
+topksid = 3; % 25,100,1000
+xlineorig = topks(topksid);
+% ylim([0,1]);
 for dataid=1:num_data
 %   xaxis = log10(maxnnzs); xline = log10(xlineorig); 
     xaxis = log10(maxnnzs./edgedensity(dataid)); xline = log10(xlineorig);
@@ -51,3 +152,12 @@ for dataid=1:num_data
 end
 hx = graph2d.constantline(xline, 'LineStyle','--','Color',[.7 .7 .7]);
 changedependvar(hx,'x');
+
+title('expmimv: Set Precision vs. log10(maxnnz/edgedensity)');
+xlabel('log10(maxnnz/edgedensity)');
+ylabel('Set Precision');
+legend('itdk0304', 'dblp-2010', 'flickr-scc', 'ljournal-2008', 'webbase-2001','twitter-2010','friendster','Location','Southeast');
+legend boxoff;
+set_figure_size(figdims);
+print(gcf,strcat('maxnnz_vs_Precision_edense','.eps'),'-depsc2');
+
--- experiments/acc_vs_maxnnz/maxnnz_process.m
+++ experiments/acc_vs_maxnnz/maxnnz_process.m
@@ -26,61 +26,62 @@ edgeden = edgedensity;
 
 numrecords = num_data;
 
-  % now load webbase
+  %% now load webbase
   	load(strcat(experiment_directory , experimentname, '_webbase'));
-  	num_newdata = size(degrees,2);
-  	acc_data(:,end:end+num_newdata,:,:) = 0; % make room in the data-log for new data
+  	numrecords = numrecords + 1;    
+  	acc_data(:,numrecords,:,:) = 0; % make room in the data-log for new data
+    num_newdata = 1;
 	for dataid=1:num_newdata
 		for maxnnzsid=1:num_maxnnzs
 			for topksid=1:num_topks
-				acc_data(1,dataid,maxnnzsid,topksid) = log10(prctile(err_vals(maxnnzsid,:,dataid),50));
-				acc_data(2,dataid,maxnnzsid,topksid) = prctile(kendall_valsnn(dataid, :, maxnnzsid, topksid),50);
-				acc_data(3,dataid,maxnnzsid,topksid) = prctile(intersect_valsnn(dataid, :, maxnnzsid, topksid),50);
+				acc_data(1,numrecords,maxnnzsid,topksid) = log10(prctile(err_vals(maxnnzsid,:,dataid),50));
+				acc_data(2,numrecords,maxnnzsid,topksid) = prctile(kendall_valsnn(dataid, :, maxnnzsid, topksid),50);
+				acc_data(3,numrecords,maxnnzsid,topksid) = prctile(intersect_valsnn(dataid, :, maxnnzsid, topksid),50);
 			end
 		end
 	end
-	edgeden(end:end+num_newdata) = edgedensity;
+	edgeden(numrecords) = edgedensity;
 	
-  	numrecords = numrecords + num_newdata;
+
 	
   	
-  % now load twitter
+  %% now load twitter
   	load(strcat(experiment_directory , experimentname, '_twitter'));
-  	num_newdata = size(degrees,2);
-  	acc_data(:,end:end+num_newdata,:,:) = 0; % make room in the data-log for new data
+  	numrecords = numrecords + 1;    
+  	acc_data(:,numrecords,:,:) = 0; % make room in the data-log for new data
+    num_newdata = 1;
 	for dataid=1:num_newdata
 		for maxnnzsid=1:num_maxnnzs
 			for topksid=1:num_topks
-				acc_data(1,dataid,maxnnzsid,topksid) = log10(prctile(err_vals(maxnnzsid,:,dataid),50));
-				acc_data(2,dataid,maxnnzsid,topksid) = prctile(kendall_valsnn(dataid, :, maxnnzsid, topksid),50);
-				acc_data(3,dataid,maxnnzsid,topksid) = prctile(intersect_valsnn(dataid, :, maxnnzsid, topksid),50);
+				acc_data(1,numrecords,maxnnzsid,topksid) = log10(prctile(err_vals(maxnnzsid,:,dataid),50));
+				acc_data(2,numrecords,maxnnzsid,topksid) = prctile(kendall_valsnn(dataid, :, maxnnzsid, topksid),50);
+				acc_data(3,numrecords,maxnnzsid,topksid) = prctile(intersect_valsnn(dataid, :, maxnnzsid, topksid),50);
 			end
 		end
 	end
-	edgeden(end:end+num_newdata) = edgedensity;
-		
-  	numrecords = numrecords + num_newdata;
+	edgeden(numrecords) = edgedensity;
   
-  % now load friendster
-  	load(strcat(experiment_directory , experimentname, '_friend'));
-  	num_newdata = size(degrees,2);
-  	acc_data(:,end:end+num_newdata,:,:) = 0; % make room in the data-log for new data
+  %% now load friendster
+  	load(strcat(experiment_directory , experimentname, '_friendster'));
+  	numrecords = numrecords + 1;    
+  	acc_data(:,numrecords,:,:) = 0; % make room in the data-log for new data
+    num_newdata = 1;
 	for dataid=1:num_newdata
 		for maxnnzsid=1:num_maxnnzs
 			for topksid=1:num_topks
-				acc_data(1,dataid,maxnnzsid,topksid) = log10(prctile(err_vals(maxnnzsid,:,dataid),50));
-				acc_data(2,dataid,maxnnzsid,topksid) = prctile(kendall_valsnn(dataid, :, maxnnzsid, topksid),50);
-				acc_data(3,dataid,maxnnzsid,topksid) = prctile(intersect_valsnn(dataid, :, maxnnzsid, topksid),50);
+				acc_data(1,numrecords,maxnnzsid,topksid) = log10(prctile(err_vals(maxnnzsid,:,dataid),50));
+				acc_data(2,numrecords,maxnnzsid,topksid) = prctile(kendall_valsnn(dataid, :, maxnnzsid, topksid),50);
+				acc_data(3,numrecords,maxnnzsid,topksid) = prctile(intersect_valsnn(dataid, :, maxnnzsid, topksid),50);
 			end
 		end
 	end
-	edgeden(end:end+num_newdata) = edgedensity;
-		
-  	numrecords = numrecords + num_newdata;
+	edgeden(numrecords) = edgedensity;
+    
 	clear edgedensity;
 	edgedensity = edgeden;
 	
-% Now acc_maxnnz contains all data for maxnnz experiments
+%% Now acc_maxnnz contains all data for maxnnz experiments
 
 save(strcat(experiment_directory, experimentname, '_to_plot', '.mat'), 'acc_data', 'edgedensity','-v7.3');
+%%
 exit
