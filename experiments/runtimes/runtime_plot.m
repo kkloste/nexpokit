6f6dd501a1e3253bfecb62eab5b99c22aa919f21
Kyle Kloster <kyle.kloster@gmail.com>
2014-04-03 10:19:26 -0400

    runtime plot aesthetic changes

--- experiments/runtimes/runtime_plot.m
+++ experiments/runtimes/runtime_plot.m
@@ -41,7 +41,7 @@ end
 title('Runtimes for tol = 1e-4');
 xlabel('log10(|V|+ nnz(P))');
 ylabel('log10(runtime) (s)');
-legend(hs,'expmv', 'half', 'gexpmq', 'gexpm', 'expmimv','Location','Southeast');
+legend(hs,'expmv', 'half', 'gexpmq', 'gexpm', 'expmimv','Location','Northwest');
 legend boxoff;
 %xlim([4.5,9.75]);
 set_figure_size([5,3]);
